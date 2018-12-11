93899308b88285d5a392a8e08c96573bff9b2169
Björn Kihlberg <bjorn.kihlberg@frontedgeit.se>
2018-12-11 13:10:45 +0100

    webpack config for build process

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/main',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: { modules: true }
+                    }
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './output/')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' })
+    ]
+}
